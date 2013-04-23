41069d4e70ace6ddd99bb2eec374bed96b2ab7e0
Cesar MC <cesarmoracastro@gmail.com>
2013-04-23 09:08:56 -0500

    FileRegister class now loads the content, and computes the checksum :)

--- DSProject3/src/FileRegister.java
+++ DSProject3/src/FileRegister.java
@@ -1,13 +1,20 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class FileRegister {
 
 	private File _file;
-	private String _checksum;
+	private byte[] _checksum;
+	private byte[] _content;
 	
 	public FileRegister(File file){
 		_file = file;
+		loadContent();
 		computeChecksum();
 	}
 	
@@ -15,17 +22,41 @@ public class FileRegister {
 		return _file.getName();
 	}
 	
-	public String getChecksum(){
+	public byte[] getChecksum(){
 		return _checksum;
 	}
 	
-	public String getContent(){
-		// TODO
-		return "TODO!!!";
+	public byte[]  getContent(){
+		return _content;
 	}
 	
 	private void computeChecksum() {
-
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+			md.update(_content);
+			_checksum = md.digest();
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("ERROR calculating the checksum of the file "+_file.getName());
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void loadContent(){
+		try {
+			FileInputStream fis = new FileInputStream(_file.getAbsoluteFile());
+			_content = new byte[(int) _file.length()];
+			fis.read(_content);
+			fis.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR loading the file "+_file.getAbsolutePath());
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("ERROR reading the file "+_file.getAbsolutePath());
+			e.printStackTrace();
+		};
+		
 	}
 	
 	public boolean equals(Object o){
